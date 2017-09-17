710939c1da94d1dba5293627529f84e956be0021
eBagaoisan <31226124+eBagaoisan@users.noreply.github.com>
2017-09-17 12:52:43 -0700

    Creating guess.cpp

---
+++ guess.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //Variable declarations
+    int randNumber;
+    int guess;
+
+    cout<<"Welcome to the guessing game! Please select a difficulty..."<<endl;
+    cout<<"Easy: 1 guess for numbers between 1-2"<<endl;
+    cout<<"Medium: 2 guesses for numbers between 1-10"<<endl;
+    cout<<"Hard:"<<endl;
+
+    return 0;
+}

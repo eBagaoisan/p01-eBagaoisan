931808612b7297227e9923f5bfd1198202886142
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-09-17 12:55:04 -0700

    Added difficulties

--- guess.cpp
+++ guess.cpp
@@ -11,9 +11,9 @@ int main(){
     int guess;
 
     cout<<"Welcome to the guessing game! Please select a difficulty..."<<endl;
-    cout<<"Easy: 1 guess for numbers between 1-2"<<endl;
-    cout<<"Medium: 2 guesses for numbers between 1-10"<<endl;
-    cout<<"Hard:"<<endl;
+    cout<<"Easy: 1 guess for either 1 or 2"<<endl;
+    cout<<"Medium: 2 guesses for a random number between 1-10"<<endl;
+    cout<<"Hard: 3 guesses for a random number between 1-100"<<endl;
 
     return 0;
 }

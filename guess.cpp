71d252eb3b57ea5f850fae1816c2b86138adbe78
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-09-19 22:09:18 -0700

    Added medium difficulty in code, still need to fix loops

--- guess.cpp
+++ guess.cpp
@@ -13,6 +13,7 @@ int main(){
     string difficulty;
     string repeatGame;
     bool guessLoop = true;
+    //bool guessTemp;
     bool gameLoop = true;
 
     cout<<endl;
@@ -23,40 +24,58 @@ int main(){
     cout<<"Hard: 3 guesses for a random number between 1-100"<<endl;
     
     //Guessing Game Loop
-    //while (gameLoop == true){
-    cout<<"Please select a difficulty: ";
-    cin>>difficulty;
+        //while (gameLoop == true){
+            cout<<"Please select a difficulty: ";
+            cin>>difficulty;
 
     //Easy
     if(difficulty == "Easy" || difficulty == "easy" || difficulty == "ez"){
-    while (guessLoop == true){
-        cout<<"Ha, you chose easy!"<<endl<< "You have a 50/50 chance, pick 1 or 2: ";
-        cin>>guess;
-        if (guess == 1 || guess == 2){ 
-            randNum = rand() % 2 + 1;
-            if (guess == randNum){
-                cout<<endl<< "Well look at you, you guessed the right number."<<endl;
-                guessLoop == false;
-            }
-            else{
-                cout<<"WRONG!!! Nice try though."<<endl;
-            }
-        }
-        else{
-            cout<<"Invalid entry, please try again."<<endl;
-        }
+    cout<<"Ha, you chose easy!"<< endl;
+        //while (guessLoop == true){
+            cout<< "You have a 50/50 chance, pick 1 or 2: ";
+            cin>>guess;
+                if (guess == 1 || guess == 2){ 
+                    randNum = rand() % 2 + 1;
+                    if (guess == randNum){
+                        cout<<endl<< "Well look at you, you guessed the right number."<<endl;
+                    }
+                    else{
+                    cout<<"WRONG!!! Nice try though."<<endl;
+                    }
+                }
+                else{
+                    cout<<"Invalid entry, please try again."<<endl;
+                }
+
     }
-    }
-/*
+    //}
+
     //Medium
+    if(difficulty=="Medium" || difficulty=="medium" || difficulty=="med" || difficulty=="Med" || difficulty== "m" || difficulty=="M"){
+    cout<<"Okay, medium. Basic choice."<<endl;
+        //while(guessLoop == true){
+            cout<<"You get 2 guesses, guess a number between 1 and 10: "<<endl;
+            cin>>guess;
+                if (guess < 11 || guess > 0){
+                    randNum = rand() % 10 + 1;
+                    if (guess == randNum){
+                        cout<<endl<<"Congratulations, "<< guess << " is correct!"<<endl;
+                    }
+                    else{
+                        cout<<"Inncorrect guess, nice try though."<<endl;
+                    }
+                }
+                else{
+                    cout<<"Invalid entry, please try again."<<endl;
+                }
+
 
     //Hard
 
     }
 
     //Guessing Game Loop Answer
-    cout<<"Would you like to play again?(Y/N): "<<endl;
-*/  
+    cout<<"Would you like to play again?(Y/N): "<<endl;  
 
     return 0;
 }
